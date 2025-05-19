5997f332432571fcccc0317d2386991ee7887450
SALAH_JNB <150282755+salah-jnb@users.noreply.github.com>
2025-05-20 00:36:20 +0100

    Update module-info.java

--- module-info.java
+++ module-info.java
@@ -1,10 +1,15 @@
-/**
- * 
- */
-/**
- * 
- */
 module Porjet {
-	requires java.sql;
-	requires java.desktop;
+    requires java.sql;
+    requires java.desktop;
+
+    // Modules Jackson
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+
+    // Exporter le package 'util' vers Jackson
+    exports util to com.fasterxml.jackson.databind;
+    exports model to com.fasterxml.jackson.databind;
+    opens util to com.fasterxml.jackson.databind;
+    opens model to com.fasterxml.jackson.databind;
 }
